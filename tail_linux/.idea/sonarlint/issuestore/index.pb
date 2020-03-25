ab2ba411e5d2e5f51c029da89c04fd9fc6b62d06
araikgevorgian <arayik.gevorgyan@yandex.ru>
2020-03-25 14:15:15 +0400

    'tail -c' command is ready

--- tail_linux/.idea/sonarlint/issuestore/index.pb
+++ tail_linux/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+S
+#src/main/java/com/example/Tail.java,d/8/d8da288d14aa7ad25581586d2e89e988caf45802
--- tail_linux/src/main/java/com/example/Tail.java
+++ tail_linux/src/main/java/com/example/Tail.java
@@ -2,10 +2,17 @@ package com.example;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Tail {
+
+    public static final String CHARSET = "UTF-32";
 
     public static void main(String[] args) {
 
@@ -19,7 +26,7 @@ public class Tail {
         switch (command) {
 
             case "-c":
-                printBytes();
+                printBytes(args);
                 break;
 
             case "-n":
@@ -65,7 +72,7 @@ public class Tail {
         System.out.println("------------------------------------------------------------");
 
 
-        if(count > queue.size()){
+        if (count > queue.size()) {
             System.out.println("\nWARNING: There are less lines in the file " + "'" + filePath + "'" +
                     " than you try to retrieve.\n");
         }
@@ -83,14 +90,66 @@ public class Tail {
                     printLinesFromOneFile(Integer.parseInt(args[1]), args[i]);
                     ++i;
                 }
-            }catch (NumberFormatException e){
-                e.printStackTrace();
+            } catch (NumberFormatException e) {
+                System.out.println("The second argument must be an integer\n\n");
+                printHelp();
             }
         } else
             printInvalidCommandMessage();
     }
 
-    private static void printBytes() {
+    private static void printBytes(String[] args) {
+        if (args.length >= 3) {
+            int i = 2;
+            try {
+                while (i < args.length) {
+                    prinBytesFromOneFile(Integer.parseInt(args[1]), args[i++]);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("The second argument must be an integer\n\n");
+                printHelp();
+            }
+        } else
+            printInvalidCommandMessage();
+    }
+
+    private static void prinBytesFromOneFile(int count, String filePath) {
+        int remainder = count % 4;
+        System.out.println("______________________________________________________________________");
+        System.out.println("Retrieving " + count + " byte characters from " + "'" + filePath + "'");
+        System.out.println("______________________________________________________________________");
+
+        try {
+            String text = new String(Files.readAllBytes(Paths.get(filePath)));
+
+            byte[] temp = text.getBytes(CHARSET);
+
+            if (count > temp.length) {
+                System.out.println("\nWARNING: There are less bytes in the file " + "'" + filePath + "'" +
+                        " than you try to retrieve.\n");
+                System.out.println(text);
+
+            } else {
+
+                byte[] tail = Arrays.copyOfRange(temp, temp.length - (count - remainder), temp.length);
+
+                if (remainder != 0) {
+
+                    byte[] firstCharacterBytes = Arrays.copyOfRange(temp, temp.length - 4 - (count - remainder),
+                            temp.length - (count - remainder));
+                    String firstCharacter = new String(firstCharacterBytes, Charset.forName(CHARSET));
+
+                    System.out.println("\nWARNING: Because the number of bytes you have entered is non dividable" +
+                            " by '4' the " + "first character may be misparsed.\nBut in any case the right first " +
+                            "character is -> " + firstCharacter + " <-\n");
+                }
+
+                System.out.println(new String(tail, Charset.forName(CHARSET)));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
